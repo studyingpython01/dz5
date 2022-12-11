29c8b6f1742cdadf55c6de906dfec8328c1fd0bd
studyingpython01 <114178003+studyingpython01@users.noreply.github.com>
2022-12-11 15:57:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import shutil
+from victory import questions as que
 import random
-  # False
-x = 8
+import platform
+import sys
 import os
 while True:
     print('- создать папку: 1')
@@ -57,62 +58,12 @@ while True:
             files.append(filename)
         print(files)
     if answer == 10:
-         something = 0
+        print(os.uname())
     if answer == 11:
         print('Сделано Васильевым Артёмом (12 лет)')
     if answer == 12:
-        while True:
+        que()
 
-            birthdays = {'napoleon': '15.08.1769',
-                         'zuckerberg': '14.05.1984',
-                         'musk': '28.06.1971',
-                         'bezos': '12.01.1964',
-                         'da_vinci': '02.04.1452',
-                         'newton': '04.01.1643',
-                         'einstein': '14.03.1879',
-                         'columbus': '31.10.1451',
-                         'paster': '27.12.1822',
-                         'darwin': '12.02.1809'
-                         }
-            answers = {
-                '15.08.1769': 'Пятнадцатое августа 1769 года',
-                '14.05.1984': 'Четырнадцатое мая 1984 года',
-                '28.06.1971': 'Двадцать восьмое июня 1971 года',
-                '12.01.1964': 'Двенадцатое января 1964 года',
-                '02.04.1452': 'Второе апреля 1452 года',
-                '04.01.1643': 'Четвёртое января 1643 года',
-                '14.03.1879': 'Четырнадцатое марта 1879 года',
-                '31.10.1451': ' Тридцать первое октября 1451 года',
-                '27.12.1822': 'Двадцать седьмое декабря 1822 года',
-                '12.02.1809': 'Двенадцатое февраля'
-            }
-            mistakes = 0
-            right = 0
-
-            keys = random.sample(list(birthdays), 5)
-            for key in list(keys):
-                answer_m = input('Укажите дату рождения(dd.mm.yyyy): ' + key + '\n')
-                if answer_m != birthdays[key]:
-                    mistakes += 1
-                    print(answers.get(birthdays[key]))
-                else:
-                    right += 1
-
-
-            def calculate(x, y, z):
-                result = x * (y / z)
-                return result
-
-
-            print('Ошибки:', mistakes)
-            print('Правильные ответы:', right)
-            right = int(right)
-            mistakes = int(mistakes)
-            print('Процент правильных ответов:', calculate(right, 100, 5))
-            print('Процент неправильных ответов:', calculate(mistakes, 100, 5))
-            answer_last = input("Хотите начать игру с начала? Если нет, введите 'нет'. Если да, введите 'да'.")
-            if answer_last == 'нет':
-                break
     if answer == 13:
         card = 0
         buyings = []
